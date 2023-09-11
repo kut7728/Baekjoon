293e95b52f9b2bbf6238876d3b4ae54fb12235b8
kut7728 <kut7728@hanyang.ac.kr>
2023-09-12 03:52:33 +0900

    9/12 03:52

--- example.py
+++ example.py
@@ -1,10 +1,4 @@
 n = int(input())
-sum = []
-num = []
 
 for i in range(n):
-  num.append(list(map(int, input().split())))
-  sum.append(num[i][0] + num[i][1])
-
-for t in range(n):
-  print(f"Case #{t+1}: {num[t][0]} + {num[t][1]} = {sum[t]}")
+  print("*" * (i+1))
