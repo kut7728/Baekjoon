c1306cdb58b47f9dea49bac384ae6c2946623b9d
kut7728 <kut7728@hanyang.ac.kr>
2023-09-18 18:12:20 +0900

    9/18 18:12

--- example.py
+++ example.py
@@ -1,14 +1,10 @@
-n, m = map(int, input().split())
-bask = [0]*n
+stud = ['False'] * 30
 
-for i in range(n):
-  bask[i] = i+1
+for i in range(28):
+  num = int(input())
+  stud[num-1] = 'True'
 
-for i in range(m):
-  a, b = map(int, input().split())
-  temp = bask[a-1]
-  bask[a-1] = bask[b-1]
-  bask[b-1] = temp
+target = list(filter(lambda x: stud[x] == 'False', range(30)))
 
-for i in range(n):
-  print(bask[i],  end=" ")
+print(min(target)+1)
+print(max(target)+1)
